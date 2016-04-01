6207366c5657214dc51ae6b2f7856609c0160648
Joseph Garcia <j.garc419@gmail.com>
2016-04-01 00:46:37 -0400

    create custom cell to show pokemon info

---
+++ webrequests/PokemonCell.swift
@@ -0,0 +1,24 @@
+//
+//  PokemonCell.swift
+//  webrequests
+//
+//  Created by Joseph Garcia on 4/1/16.
+//  Copyright © 2016 joebeard. All rights reserved.
+//
+
+import UIKit
+
+class PokemonCell: UICollectionViewCell {
+    
+    @IBOutlet weak var pokemonImg: UIImageView!
+    @IBOutlet weak var pokemonName: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        pokemonName.text = self.pokemon.name.capitalizedString
+        pokemonImg.image = UIImage(named: "\(self.pokemon.pokemonId)")
+    }
+}
